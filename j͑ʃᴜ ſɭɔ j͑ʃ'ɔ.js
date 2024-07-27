dd6bb13765c70e098418e77f10fd3c97aa081b41
Krhitlashi <krhitlashi@outlook.com>
2024-07-27 09:40:51 -0700

    ſ̀ȷᴜ ſɭᴜꞇ

--- j͑ʃᴜ ſɭɔ j͑ʃ'ɔ.js
+++ j͑ʃᴜ ſɭɔ j͑ʃ'ɔ.js
@@ -1,19 +1,47 @@
-var staticCacheName = "pwa";
- 
+var staticCacheName = "pwa-v1";
+var assetsToCache = [
+  "/",
+  "/֭ſɭᴜ ı],ɔ.css",
+  "/index.html",
+  "/ſ͔ɭᴜ ᶅſɔ.html",
+  "/ꞁȷ̀ᴜ ɽ͑ʃ'ᴜ ֭ſɭᴜ ı],ɹ.html",
+  "/ꞁȷ̀ꞇ }ʃᴜƽ.html"
+];
+
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(assetsToCache);
     })
   );
 });
- 
+
 self.addEventListener("fetch", function (event) {
   console.log(event.request.url);
- 
+
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(function (response) {
+        return caches.open(staticCacheName).then(function (cache) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  var cacheWhitelist = [staticCacheName];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
