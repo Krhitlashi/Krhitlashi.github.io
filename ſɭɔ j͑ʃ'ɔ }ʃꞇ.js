dc641f5b97d965e2701e88cf6349199220b0b61c
Krhitlashi <krhitlashi@outlook.com>
2025-07-09 16:30:58 -0700

    Saka Vacepaini

--- ſɭɔ j͑ʃ'ɔ }ʃꞇ.js
+++ ſɭɔ j͑ʃ'ɔ }ʃꞇ.js
@@ -23,26 +23,24 @@ function vacepai(s2haxe) {
   hatarakef.forEach(arakef => {
     const tsiqaiOx2 = arakef.textContent.trim();
     arakef.innerHTML = "";
-    
-    const xezCa1ara = tsiqaiOx2.split(/([ \n])/);
 
     let kaltokani = document.createElement("div");
     kaltokani.className = "cepaifalkek";
     arakef.appendChild(kaltokani);
 
-    xezCa1ara.forEach(xez => {
-      if (xez === "\n") {
-        kaltokani = document.createElement("div");
-        kaltokani.className = "cepaifalkek";
-        arakef.appendChild(kaltokani);
-      } else if (xez === " ") {
-        kaltokani.appendChild(document.createTextNode(" "));
-      } else if (xez) {
-        const xezKek = document.createElement("div");
-        xezKek.className = "cepaifalkef";
-        xezKek.textContent = xez;
-        kaltokani.appendChild(xezKek);
-      }
+    tsiqaiOx2.split("\n").forEach((cepaini, ruva) => {
+      if (ruva > 0) kaltokani.appendChild(document.createElement("br"));
+      cepaini.split(" ").forEach((xez, xezruva, xezrakaz) => {
+        if (xez) {
+          const xezKek = document.createElement("div");
+          xezKek.className = "cepaifalkef";
+          xezKek.textContent = xez;
+          kaltokani.appendChild(xezKek);
+        }
+        if (xezruva < xezrakaz.length - 1) {
+          kaltokani.appendChild(document.createTextNode(" "));
+        }
+      });
     });
   });
 }
