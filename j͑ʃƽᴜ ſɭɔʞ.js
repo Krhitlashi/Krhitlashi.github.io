d065593fe4f193f9ac3db5903aed11610edad12d
Krhitlashi <krhitlashi@outlook.com>
2025-11-15 10:43:33 -0800

    Skakef

---
+++ j͑ʃƽᴜ ſɭɔʞ.js
@@ -0,0 +1,25 @@
+const oskakefani = document.querySelectorAll("[data-oskakefani]");
+
+const ruvaS2karani = new URLSearchParams(window.location.search);
+let kjesaiGawe = ruvaS2karani.get("lang") || "aih";
+k2regawe(kjesaiGawe);
+
+function k2regawe(gawe) {
+    kocepaifal(gawe)
+    document.documentElement.setAttribute("lang", gawe)
+    oskakefani.forEach(kek => {
+        const kef = kek.dataset.oskakefani;
+        kek.textContent = skakefani[gawe][kef] || skakefani["aih"][kef] || kef;
+    });
+    document.querySelectorAlla=("a[href]").forEach(cel2f => {
+        const ruva = new URL(cel2f.href, window.location.href);
+        ruva.searchParams.set("lang", gawe);
+        cel2f.href = ruva.toString();
+    });
+}
+function kocepaifal(gawe) {
+    if (gawe != "aih") {
+        document.body.classList.remove("cepaifal")
+        document.body.classList.add("tapaifal")
+    }
+}
